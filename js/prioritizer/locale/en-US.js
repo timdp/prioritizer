4d72356247086fe0a78a6bf4fd5e9fc2221c066f
Tim De Pauw <tim@pwnt.be>
2013-01-09 00:09:39 +0100

    Indenting locales with spaces

--- js/prioritizer/locale/en-US.js
+++ js/prioritizer/locale/en-US.js
@@ -1,11 +1,11 @@
 Prioritizer.locale = {
-	instructions:
-		"Drag the options in your preferred order."
-			+ " Your choice is automatically saved.",
-	error_invalid_response:
-		"The server returned an invalid response.",
-	error_request_failed:
-		"The server could not be contacted.",
-	error_start_failed:
-		"The application could not be started."
+    instructions:
+        "Drag the options in your preferred order."
+            + " Your choice is automatically saved.",
+    error_invalid_response:
+        "The server returned an invalid response.",
+    error_request_failed:
+        "The server could not be contacted.",
+    error_start_failed:
+        "The application could not be started."
 };
