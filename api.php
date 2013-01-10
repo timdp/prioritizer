c6eda50043c78beb863f94cb80397de4f3604a56
Tim De Pauw <tim@pwnt.be>
2013-01-10 16:07:26 +0100

    Spaces are cooler than tabs, apparently

--- api.php
+++ api.php
@@ -15,47 +15,47 @@ $choice_file = "data/$user_id.txt";
 
 $result = array();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$choices = array_map('intval', explode(',', $_POST['order']));
-	if (valid_choices($choices, count($options))) {
-		save_choices($choices, $choice_file);
-		$result['success'] = true;
-	}
+    $choices = array_map('intval', explode(',', $_POST['order']));
+    if (valid_choices($choices, count($options))) {
+        save_choices($choices, $choice_file);
+        $result['success'] = true;
+    }
 } else {
-	$choices = get_choices($choice_file);
-	if (is_array($choices)) {
-		$result['order'] = $choices;
-	}
-	$result['options'] = $options;
+    $choices = get_choices($choice_file);
+    if (is_array($choices)) {
+        $result['order'] = $choices;
+    }
+    $result['options'] = $options;
 }
 
 header('Content-Type: application/json');
 echo json_encode($result);
 
 function save_choices(&$choices, $choice_file) {
-	$fp = fopen($choice_file, 'w');
-	for ($i = 0; $i < count($choices); ++$i) {
-		fwrite($fp, $choices[$i] . PHP_EOL);
-	}
-	fclose($fp);
-	chmod($choice_file, 0644);
+    $fp = fopen($choice_file, 'w');
+    for ($i = 0; $i < count($choices); ++$i) {
+        fwrite($fp, $choices[$i] . PHP_EOL);
+    }
+    fclose($fp);
+    chmod($choice_file, 0644);
 }
 
 function get_choices($choice_file) {
-	if (file_exists($choice_file)) {
-		$choices = array_map('rtrim', file($choice_file));
-		return array_map('intval', $choices);
-	}
-	return null;
+    if (file_exists($choice_file)) {
+        $choices = array_map('rtrim', file($choice_file));
+        return array_map('intval', $choices);
+    }
+    return null;
 }
 
 function valid_choices($choices, $num_options) {
-	if (count($choices) != $num_options) {
-		return false;
-	}
-	sort($choices, SORT_NUMERIC);
-	$i = 0;
-	while ($i < $num_options && $choices[$i] == $i + 1) {
-		++$i;
-	}
-	return ($i == $num_options);
+    if (count($choices) != $num_options) {
+        return false;
+    }
+    sort($choices, SORT_NUMERIC);
+    $i = 0;
+    while ($i < $num_options && $choices[$i] == $i + 1) {
+        ++$i;
+    }
+    return ($i == $num_options);
 }
